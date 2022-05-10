3532ab2f0f01d36329dcd67f38da00319c9e2acc
denzel <dguchinsky@yahoo.com>
2022-05-10 06:11:54 -0500

    Declare task5.3 functions

---
+++ DmitriGucinski/Task5.3.py
@@ -0,0 +1,9 @@
+
+def get_top_performers(file_path, number_of_top_students=5):
+    '''Receives file path and returns names of top performer students'''
+    pass
+    
+
+def create_ordered_file(file_path, order=desc):
+    '''Receives the file path with students info and writes CSV student information to the new file in descending order of age.'''
+    pass
