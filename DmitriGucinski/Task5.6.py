270834f9b2d155bb8db6340f8d06602feac6d872
denzel <dguchinsky@yahoo.com>
2022-05-14 05:00:37 -0500

    Task5.6 done

--- DmitriGucinski/Task5.6.py
+++ DmitriGucinski/Task5.6.py
@@ -1,6 +1,13 @@
 
 def call_once(func):
     '''runs a function or method once and caches the result. All consecutive calls to this function should return cached result no matter the arguments.'''
+    cache = 0
+    def wrapper(a, b):
+        nonlocal cache
+        if not cache: 
+            cache = a + b        
+        return cache
+    return wrapper    
     
     
 @call_once
